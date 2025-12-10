f7255aa5c300cb38df3fb653936cd3547d8e796f
Jolly Gupta <coolart25@gmail.com>
2025-12-10 14:22:57 -0800

    async

---
+++ GettingStartedAsyncAwait/GettingStartedAsyncAwait/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  GettingStartedAsyncAwait
+//
+//  Created by Mohammad Azam on 7/9/21.
+//
+
+import SwiftUI
+
+struct CurrentDate: Decodable, Identifiable {
+    let id = UUID()
+    let date: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case date = "date"
+    }
+}
+
+struct ContentView: View {
+
+    var body: some View {
+        NavigationView {
+            List(1...10, id: \.self) { index in
+                Text("\(index)")
+            }.listStyle(.plain)
+            
+            .navigationTitle("Dates")
+            .navigationBarItems(trailing: Button(action: {
+                // button action
+            }, label: {
+                Image(systemName: "arrow.clockwise.circle")
+            }))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
